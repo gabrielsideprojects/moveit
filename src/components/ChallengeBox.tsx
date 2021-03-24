db8fbbd15ccc11d5f688d99c5bbe337e116d882f
gabrielsideprojects <gabrielsideprojects@gmail.com>
2021-03-24 11:51:52 -0300

    Criado o componente de ChallengeBox, que serve para mostrar o desafio a cada vez que o timer é finalizado

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,3 +1,36 @@
+import styles from '../styles/components/ChallengeBox.module.css'
+
+
 export function ChallengeBox() {
-    return ()
+    const hasActiveChallenge = true;
+
+    return (
+        <div className={styles.challengeBoxContainer}>
+            {
+                hasActiveChallenge ? (
+                    <div className={styles.challengeActive}>
+                        <header>Ganhe 400 xp</header>
+                        <main>
+                            <img src="icons/body.svg"/>
+                            <strong>Novo desafio</strong>
+                            <p>Levante e faça uma caminha de 3 minutos</p>
+                        </main>
+                        <footer>
+                            <button type="button" className={styles.challengeFailedButton}>Falhei</button>
+                            <button type="button" className={styles.challengeSucceededButton}>Completei</button>
+                        </footer>
+                    </div>
+                ) : (
+                    <div className={styles.challengeNotActive}>
+                        <strong>Finalize um ciclo para receber um desafio</strong>
+                        <p>
+                            <img src="icons/level-up.svg" alt="Level Up"/>
+                            Avance de level completando desafios.
+                        </p>
+                    </div>
+                )
+            }
+           
+        </div>
+    )
 }
