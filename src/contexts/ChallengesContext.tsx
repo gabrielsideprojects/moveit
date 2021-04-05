5f8b8a6a28dc6af3520169f905a24bfa21b7f7c0
gabrielsideprojects <gabrielsideprojects@gmail.com>
2021-04-05 13:20:37 -0300

    Add notificacoes

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import {createContext, useState, ReactNode} from 'react'
+import {createContext, useState, ReactNode, useEffect} from 'react'
 import challenges from '../../challenges.json'
 
 interface Challenge {
@@ -32,6 +32,11 @@ export function ChallengesProvider({children}: ChallengesProviderProps) {
     const [challengesCompleted, setChanllengesCompleted] = useState(0)
     const [activeChallenge , setActiveChallenge]= useState(null)
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
+
+    useEffect(()=>{
+        Notification.requestPermission()
+    }, [])
+
     function levelUp(){
         setLevel(level + 1)
     }
@@ -40,6 +45,13 @@ export function ChallengesProvider({children}: ChallengesProviderProps) {
         const randomChallengedIndex = Math.floor(Math.random() * challenges.length)
         const challenge = challenges[randomChallengedIndex]
         setActiveChallenge(challenge)
+        new Audio('/notification.mp3').play()
+        if(Notification.permission === 'granted') {
+            new Notification('Novo desafio', {
+                body: `Valendo ${challenge.amount}xp`,
+                
+            })
+        }
     }
 
     function resetChallenge(){
